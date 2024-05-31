d68336008a53121cccf090ad1dd60030f2bb9345
fatush254 <fatuma.galgalo@student.moringaschool.com>
2024-05-31 11:03:49 +0300

    completed

--- lib/debug.py
+++ lib/debug.py
@@ -4,3 +4,17 @@ from dog import Dog
 import ipdb
 
 ipdb.set_trace()
+
+
+dog = Dog("Fido", "Corgi")
+
+
+try:
+    dog.name = 7  
+except ValueError as e:
+    print("Caught ValueError:", e)
+
+try:
+    dog.breed = "Poodle"  
+except ValueError as e:
+    print("Caught ValueError:", e)
